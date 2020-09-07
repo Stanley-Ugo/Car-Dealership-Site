a770b69c9684417a5dd27716e25a87da9316718a
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-09-07 19:40:35 +0100

    adding a car class & constructor

--- js/script.js
+++ js/script.js
@@ -8,5 +8,17 @@ const CreateCars = (() => {
     const cars = [];
 
     //car class
-    
+    class Car {
+        constructor(make, country, img, special, model, price, type, trans, gas) {
+            this.make = make;
+            this.country = country;
+            this.img = img;
+            this.special = special;
+            this.model = model;
+            this.price = price;
+            this.type = type;
+            this.trans = trans;
+            this.gas = gas;
+        }     
+    }
 })();

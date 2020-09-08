e78de259d57e03ec30867872802428aba75435ef
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-09-08 18:42:42 +0100

    adding the filter functions to the buttons

--- js/script.js
+++ js/script.js
@@ -137,7 +137,24 @@ const DisplayCars = ((CreateCars) => {
 })(CreateCars);
 
 //filter cars
-const FilterCars (() => {
+const FilterCars = (() => {
+    const filter = document.querySelectorAll('.filter-btn');
 
+    filter.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            const value = event.target.dataset.filter;
+            const singleCar = document.querySelectorAll('.single-car');
+
+            singleCar.forEach((car) => {
+                if(value === 'all'){
+                    car.style.display = 'block';
+                }
+                else {
+                    (!car.classList.contains(value)) ? car.style.display = 'none' 
+                    : car.style.display = 'block';
+                }
+            })
+        })
+    })
 })();
 //show modal
